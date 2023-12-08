af6624a2c3f449c68a7b72186a046affe707345c
Albirth <albertmirasoljr@gmail.com>
2023-12-08 22:58:50 +0800

    base edited

--- cardquest/views.py
+++ cardquest/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic.list import ListView
-from cardquest.models import PokemonCard, Trainer
+from cardquest.models import PokemonCard, Trainer, Collection
 
 class TrainerList(ListView):
     model = Trainer
@@ -11,10 +11,25 @@ class TrainerList(ListView):
     template_name = 'trainers.html'
     paginate_by = 15
 
+class Collection(ListView):
+    model = Collection
+    context_object_name = 'collection'
+    template_name = 'collection.html'
+    paginate_by = 15
+
 class HomePageView(ListView):
     model = PokemonCard
     context_object_name = 'home'
     template_name = "base.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+    
+class PokemonCard(ListView):
+    model = PokemonCard
+    context_object_name = 'casrd'
+    template_name = "pokemon-card.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
